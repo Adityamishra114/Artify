27bb439c3db9bec6697f0dadfdfe5732817a5783
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2023-09-25 00:55:48 +0530

    artify project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Art Gallery App
+
+This App is built using Html,css and javaScript.
+
+**Features**
+
+This is simple art gallery design where you can go through different section of Art work and where you can hover on card and see the art details.
+
+>[Working Demo](https://adityamishra114.github.io/Artify/)
+
+### Instruction
+
+1. Clone the repo and run ``npm install``
+2. From the app folder run ``npm run dev``
---
+++ src/js/data.js
@@ -0,0 +1,56 @@
+const visualArts = [
+  {
+    id: 1,
+    userName: "alienwannabe",
+    artist: "Matthieu Comoy",
+    image:
+      "https://images.unsplash.com/photo-1501084817091-a4f3d1d19e07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    publish: "July 26, 2017",
+    gender: "Male",
+  },
+  {
+    id: 2,
+    userName: "@tanywu4ka",
+    artist: "Tetiana SHYSHKINA",
+    image:
+      "https://images.unsplash.com/photo-1589996448606-27d38c70f3bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    publish: "May 20, 2020",
+    gender: "Female",
+  },
+  {
+    id: 3,
+    userName: "@local.arts_",
+    artist: "Tabitha Turner ",
+    image:
+      "https://images.unsplash.com/photo-1593382067395-ace3045a1547?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80",
+    publish: "June 29, 2020",
+    gender: "Female",
+  },
+  {
+    id: 4,
+    userName: "supermartnuernberg",
+    artist: "Markus Spiske",
+    image:
+      "https://images.unsplash.com/photo-1559537696-55ba84f4ded2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    publish: "June 3, 2019",
+    gender: "Male",
+  },
+  {
+    id: 5,
+    userName: "sigmund",
+    artist: "Sigmund",
+    image:
+      "https://images.unsplash.com/photo-1610926950521-ab7e8b45f377?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1478&q=80",
+    publish: "January 18, 2021",
+    gender: "Male",
+  },
+  {
+    id: 6,
+    userName: "polarmermaid",
+    artist: "Anne Nygård",
+    image:
+      "https://images.unsplash.com/photo-1619879030396-cd0276f84a7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    publish: "May 1, 2021",
+    gender: "Female",
+  },
+];
